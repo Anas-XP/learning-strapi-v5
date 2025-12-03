ec103229d4be4bf3be0b3a794068c946856b8580
Anas Okasha <anas@bold-xp.com>
2025-12-03 14:26:58 +0200

    fix: update JWT management settings and refresh token lifespans; bump Strapi version to 5.31.2

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,4 +1,4 @@
-export default () => ({
+export default ({ env }) => ({
   "auto-slug-manager": {
     enabled: true,
     config: {
@@ -16,26 +16,26 @@ export default () => ({
       },
     },
   },
-    'users-permissions': {
+  "users-permissions": {
     config: {
-      jwtManagement: 'refresh',
+      jwtManagement: "refresh",
       sessions: {
         accessTokenLifespan: 604800, // 1 week (default)
         maxRefreshTokenLifespan: 2592000, // 30 days
         idleRefreshTokenLifespan: 604800, // 7 days
         httpOnly: false, // Set to true for HTTP-only cookies
         cookie: {
-          name: 'strapi_up_refresh',
-          sameSite: 'lax',
-          path: '/',
-          secure: false, // true in production
+          name: "strapi_up_refresh",
+          sameSite: "lax",
+          path: "/",
+          secure: env("NODE_ENV", "development") === "production", // true in production
         },
       },
       ratelimit: {
         enabled: true,
         interval: { min: 5 },
         max: 5,
-      }
+      },
     },
   },
 });
